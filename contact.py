1e616b2baff0176416b39559725b99d873e41a33
Tony Li <litony1999@gmail.com>
2019-02-26 17:22:50 -0500

    attemped peewee database

--- contact.py
+++ contact.py
@@ -1,55 +1,112 @@
-class Contact:
+# class Contact:
+#
+#   contacts = []
+#   next_id = 1
+#
+#   def __init__(self, first_name, last_name, email, note):
+#     """This method should initialize the contact's attributes"""
+#     self.first_name = first_name
+#     self.last_name = last_name
+#     self.email = email
+#     self.note = note
+#     self.id = Contact.next_id
+#     Contact.next_id += 1
+#
+#
+#   @classmethod
+#   def create(cls, first_name, last_name, email, note):
+#     """This method should call the initializer,
+#     store the newly created contact, and then return it
+#     """
+#     new_contact = Contact(first_name, last_name, email, note)
+#     Contact.contacts.append(new_contact)
+#     return new_contact
+#
+#   @classmethod
+#   def all(cls):
+#     """This method should return all of the existing contacts"""
+#
+#   @classmethod
+#   def find(cls):
+#     """ This method should accept an id as an argument
+#     and return the contact who has that id
+#     """
+#
+#   def update(self):
+#     """ This method should allow you to specify
+#     1. which of the contact's attributes you want to update
+#     2. the new value for that attribute
+#     and then make the appropriate change to the contact
+#     """
+#
+#
+#   @classmethod
+#   def find_by(cls):
+#     """This method should work similarly to the find method above
+#     but it should allow you to search for a contact using attributes other than id
+#     by specifying both the name of the attribute and the value
+#     eg. searching for 'first_name', 'Betty' should return the first contact named Betty
+#     """
+#
+#
+#   @classmethod
+#   def delete_all(cls):
+#     """This method should delete all of the contacts"""
+#
+#
+#   def full_name(self):
+#     """Returns the full (first and last) name of the contact"""
+#
+#
+#   def delete(self):
+#     """This method should delete the contact
+#     HINT: Check the Array class docs for built-in methods that might be useful here
+#     """
+#
+#   # Feel free to add other methods here, if you need them.
+#
+# contact1 = Contact.create('Tony', 'Li', 'litony1999@gmail.com', 'note')
+# contact2 = Contact.create('Pham', 'Geh', 'Pham1345@gmail.com', 'sup fam')
+# contact3 = Contact.create('Bill', 'Lee', 'Billy1989@gmail.com', 'hello it is me')
+# Contact.create('h', 'l', '@gm', "leettter")
+#
+# print(len(Contact.contacts))
+# list = Contact.contacts
+# print(list)
+# print(list[0].first_name)
 
-  def __init__(self):
-    """This method should initialize the contact's attributes"""
+from peewee import SqliteDatabase, Model, CharField, TextField
 
+db = SqliteDatabase('crm.db')
 
-  @classmethod
-  def create(cls):
-    """This method should call the initializer,
-    store the newly created contact, and then return it
-    """
+class Contact(Model):
+  first_name = CharField()
+  last_name = CharField()
+  email = CharField()
+  note = TextField()
 
-  @classmethod
-  def all(cls):
-    """This method should return all of the existing contacts"""
-
-  @classmethod
-  def find(cls):
-    """ This method should accept an id as an argument
-    and return the contact who has that id
-    """
-
-  def update(self):
-    """ This method should allow you to specify
-    1. which of the contact's attributes you want to update
-    2. the new value for that attribute
-    and then make the appropriate change to the contact
-    """
-
-
-  @classmethod
-  def find_by(cls):
-    """This method should work similarly to the find method above
-    but it should allow you to search for a contact using attributes other than id
-    by specifying both the name of the attribute and the value
-    eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-    """
-
-
-  @classmethod
-  def delete_all(cls):
-    """This method should delete all of the contacts"""
-
+  class Meta:
+    database = db
 
   def full_name(self):
-    """Returns the full (first and last) name of the contact"""
+    return "{} {}".format(self.first_name, self.last_name)
+
+db.connect()
+db.create_tables([Contact])
 
 
-  def delete(self):
-    """This method should delete the contact
-    HINT: Check the Array class docs for built-in methods that might be useful here
-    """
 
-  # Feel free to add other methods here, if you need them.
 
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- crm.py
+++ crm.py
@@ -1,25 +1,101 @@
 from contact import Contact
 
 class CRM:
+  def main_menu(self):
+      while True:
+          self.print_main_menu()
+          user_selected = int(input())
+          if user_selected > 6:
+              print('Number is out of range')
+          self.call_option(user_selected)
 
-  # def main_menu(self):
-  #
-  #
-  # def print_main_menu(self):
-  #
-  #
-  # def call_option(self):
-  #
-  #
-  # def add_new_contact(self):
-  #
-  #
-  # def modify_existing_contact(self):
-  #
-  #
-  # def delete_contact(self):
-  #
-  #
-  # def display_all_contacts(self):
-  #
-  # def search_by_attribute(self):
+
+  def print_main_menu(self):
+      print('[1] Add a new contact')
+      print('[2] Modify an existing contact')
+      print('[3] Delete a contact')
+      print('[4] Display all the contacts')
+      print('[5] Search by attribute')
+      print('[6] Exit')
+      print('Enter a number: ')
+
+  def call_option(self, user_selected):
+      if user_selected == 1:
+          self.add_new_contact()
+      elif user_selected == 2:
+          self.modify_existing_contact()
+      elif user_selected == 3:
+          self.delete_contact()
+      elif user_selected == 4:
+          self.display_all_contacts()
+      elif user_selected == 5:
+          self.search_by_attribute()
+      elif user_selected == 6:
+          exit()
+  def add_new_contact(self):
+      print('Enter First Name')
+      first_name = input()
+      print('Enter Last Name')
+      last_name = input()
+      print('Enter Email')
+      email = input()
+      print('Enter note')
+      note = input()
+
+      Contact.create(first_name, last_name, email, note)
+
+  def modify_existing_contact(self):
+      print('Who would you like to edit?')
+      contact_list = Contact.contacts
+      for idx, contact in enumerate(contact_list):
+          print(f"[{idx}] {contact.first_name}")
+          # need to add a way to keep repeating this so we dont have to go back to main menu everytime
+          # if i > len(contact_list):
+          #     i = 0
+      request = int(input())
+      print('You have selected user ' + contact_list[request].first_name + ' ' + contact_list[request].last_name)
+      print('What would you like to change?')
+      print('[0] First Name')
+      print('[1] Last Name')
+      print('[2] Email')
+      print('[3] note')
+      request_change = int(input())
+      if request_change == 0:
+          print("What would you like to change {} {}'s name to?'".format(contact_list[request].first_name, contact_list[request].last_name))
+          changed = input()
+          contact_list[request].first_name = changed
+      elif request_change == 1:
+          print("What would you like to change {} {}'s last name to?'".format(contact_list[request].first_name, contact_list[request].last_name))
+          changed = input()
+          contact_list[request].last_name = changed
+      elif request_change == 2:
+          print("What would you like to change {} {}'s email to?'".format(contact_list[request].first_name, contact_list[request].last_name))
+          changed = input()
+          contact_list[request].email = changed
+      elif request_change == 3:
+          print("What would you like to change {} {}'s note to?'".format(contact_list[request].first_name, contact_list[request].last_name))
+          changed = input()
+          contact_list[request].note = changed
+      else:
+          print("Please enter a valid number")
+
+  def delete_contact(self):
+      contact_list = Contact.contacts
+      print('Who would you like to remove?')
+      for idx, contact in enumerate(contact_list):
+          print(f"[{idx}] {contact.first_name}")
+      choice = int(input())
+      print('{} has been removed'.format(contact_list[choice].first_name))
+      del contact_list[choice]
+
+
+  def display_all_contacts(self):
+      contact_list = Contact.contacts
+      for people in contact_list:
+          print(people.first_name + ' ' + people.last_name + '    ' + people.email + '    ' + people.note)
+
+  def search_by_attribute(self):
+      pass
+
+a_crm_app = CRM()
+a_crm_app.main_menu()
